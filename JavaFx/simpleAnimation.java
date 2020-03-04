619bfd0dbc9aeee06c540b3454190875408728ec
henrique-temponi <tanzenheavely@gmail.com>
2020-03-04 07:56:18 -0300

    Adiciona circulos a aplicacao

--- JavaFx/simpleAnimation.java
+++ JavaFx/simpleAnimation.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 /**
@@ -13,6 +15,18 @@ public class simpleAnimation extends Application {
     public void start(Stage primaryScene)
     {
         Group gp = new Group();
+
+        Group circles = new Group();
+        for (int i = 0; i < 30; i++) {
+            Circle circle = new Circle(150, Color.web("white", 0.05));
+            circle.setStrokeType(StrokeType.OUTSIDE);
+            circle.setStroke(Color.web("white", 0.16));
+            circle.setStrokeWidth(4);
+            circles.getChildren().add(circle);
+        }
+
+        gp.getChildren().addAll(circles);
+
         Scene scene = new Scene (gp, 800, 600, Color.BLACK);
         
         primaryScene.setScene(scene);
